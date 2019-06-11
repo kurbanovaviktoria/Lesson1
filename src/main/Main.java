610bfe995bd2922ce6fc87b8346d7dc22044f93c
lapa <ho4u.vika2016@yandex.ru>
2019-06-11 18:54:58 +0300

    ,jgjhg,jgjhgkjhg

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,7 +2,7 @@ package main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Russia");
+        System.out.println("Ave");
         System.out.println("Hello World");
     }
 }
